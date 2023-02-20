ed46a0b78d381830bf69da0a59e0887072900db4
Moritz <96340080+mol42069@users.noreply.github.com>
2023-02-20 11:39:45 +0100

    Create Rectangle.cpp

---
+++ cpp/src/Rectangle.cpp
@@ -0,0 +1,20 @@
+
+class Rectangle {
+
+	bool is_full = false;
+	int x, y;
+	int width = 60, height = 60;
+
+	void init(int x, int y) {
+		this->is_full = is_full;
+		this->x = x * width + 5; 
+		this->y = y * height + 5;
+
+		# we have to make the square show the square sprite at x y
+
+		return;
+	};
+
+
+
+};
